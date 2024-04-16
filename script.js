cfd242f8875e0af78383858d2a1ade094f4e6351
sai <nsaic1425@gmail.com>
2024-04-16 19:19:19 +0530

    added functionality to button

--- script.js
+++ script.js
@@ -10,6 +10,31 @@ function displayUser(user){
         document.body.style.backgroundColor = '#fe8ca0';
         else document.body.style.backgroundColor = '#3b82f6';
     
+        userDisplay.innerHTML = ` <div class="flex justify-between">
+        <div class="flex">
+          <img
+            class="w-48 h-48 rounded-full mr-8"
+            src=${user.picture.medium}
+          />
+          <div class="space-y-3">
+            <p class="text-xl">
+              <span class="font-bold">Name : </span>${user.name.first} ${user.name.last}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Email: </span> ${user.email}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Phone: </span> ${user.phone}
+            </p>
+            <p class="text-xl">
+              <span class="font-bold">Location: </span> ${user.location.country}
+            </p>
+            <p class="text-xl"><span class="font-bold">Age: </span> ${user.dob.age}</p>
+          </div> 
+        </div>`
 }
 
+document.querySelector('#generate').addEventListener('click', fetchUser)
+
 fetchUser()
+
